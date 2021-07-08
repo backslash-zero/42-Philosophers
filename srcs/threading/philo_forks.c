31f5ce26ffe10015464e5ecc0ad40565f1bbdec8
Celestin MEUNIER <cmeunier@e2r8p13.42.fr>
2021-07-08 15:38:01 +0200

    added pickup fork print

--- srcs/threading/philo_forks.c
+++ srcs/threading/philo_forks.c
@@ -6,7 +6,7 @@
 /*   By: cmeunier <cmeunier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 18:23:38 by cmeunier          #+#    #+#             */
-/*   Updated: 2021/07/07 18:23:57 by cmeunier         ###   ########.fr       */
+/*   Updated: 2021/07/08 15:32:52 by cmeunier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ void	drop_forks(t_philosopher *philosopher)
 	pthread_mutex_unlock(philosopher->fork_right);
 }
 
-void	pickup_forks(t_philosopher *philosopher)
+void	pickup_forks(t_philosopher *philo)
 {
-	pthread_mutex_lock(philosopher->fork_left);
-	pthread_mutex_lock(philosopher->fork_right);
+	pthread_mutex_lock(philo->fork_left);
+	printtime(get_time(), philo->id, "has taken a fork", philo->settings);
+	pthread_mutex_lock(philo->fork_right);
+	printtime(get_time(), philo->id, "has taken a fork", philo->settings);
 }
